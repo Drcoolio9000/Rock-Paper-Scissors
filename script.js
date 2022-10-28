dff2ce2f50de6ca9a5e5c2859cfabfce51b81f1b
Drcoolio9000 <100106964+Drcoolio9000@users.noreply.github.com>
2022-10-28 19:13:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var you;
+var yourScore = 0;
+var opponent;
+var opponentScore = 0;
+
+var choices = ["rock", "paper", "scissors"];
+
+window.onload = function a() {
+    for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+}
+
+function selectChoice() {
+    you = this.id;
+    document.getElementById("your-choice").src = you + ".png"
+
+    //random for opponent
+    opponent = choices[Math.floor(Math.random() * 3)];
+    document.getElementById("opponent-choice").src = opponent + ".png";
+
+    //check for winner
+    if (you == opponent) {
+        yourScore += 1;
+        opponentScore += 1;
+    }
+    else if (you == "rock" && opponent == "scissors") {
+        yourScore += 1;
+    }
+    else if (you == "paper" && opponent == "rock") {
+        yourScore += 1;
+    }
+    else if (you == "scissors" && opponent == "paper") {
+        yourScore += 1;
+    }
+    else opponentScore += 1;
+
+    document.getElementById("your-score").innerText = yourScore;
+    document.getElementById("opponent-score").innerText = opponentScore;
+}
